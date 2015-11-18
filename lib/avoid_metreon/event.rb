89aae491f38f0fb77ed45df9cd71b46877c63e11
Brian Upton <ystros@gmail.com>
2015-11-17 16:32:36 -0800

    Render appropriate card by event status.
    Each possible event status has been split into its own partial file, and
    is rendered based on the status of the event passed into the renderer.

--- lib/avoid_metreon/event.rb
+++ lib/avoid_metreon/event.rb
@@ -1,6 +1,7 @@
 module AvoidMetreon
   # Represents an event, including its name, start and end dates.
   class Event
+    DATE_FORMAT = '%B %d'
     attr_reader :name, :start_date, :end_date, :url
 
     def initialize(name:, start_date:, end_date:, url:)
@@ -12,6 +13,16 @@ module AvoidMetreon
 
     def in_range?(date)
       (start_date..end_date).cover?(date)
+    end
+
+    def date_range
+      if start_date == end_date
+        start_date.strftime(DATE_FORMAT)
+      else
+        [start_date, end_date]
+          .map { |d| d.strftime(DATE_FORMAT) }
+          .join(' to ')
+      end
     end
 
     def status
